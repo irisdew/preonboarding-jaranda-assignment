6111ccc146bda000f50387291c73ae5fa23670a8
psy-g <psykyg@gmail.com>
2021-08-03 13:24:10 +0900

    feature: 검색(기능)

--- src/Pages/Admin/Admin.js
+++ src/Pages/Admin/Admin.js
@@ -1,5 +1,54 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
+import styled from 'styled-components/macro'
+
+import Search from 'Components/Admin/Search'
 
 export default function Admin() {
-  return
+  const [userInfo, setUserInfo] = useState([])
+  const searchRef = useRef()
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const fetchData = () => {
+    fetch('http://localhost:3000/data/users.json')
+      .then((res) => res.json())
+      .then((res) => {
+        setUserInfo(res)
+      })
+  }
+
+  const filterUserInfo = (selected) => {
+    let inputValue = searchRef.current.value
+    let filtering = ''
+    let dataFilter = []
+    fetchData()
+
+    // if (e.target.nextElementSibling) {
+    if (inputValue.length > 0 && selected !== '선택') {
+      // const inputValue = e.target.nextElementSibling.value
+
+      if (selected === '아이디') filtering = 'email'
+      if (selected === '이름') filtering = 'name'
+      if (selected === '나이') {
+        filtering = 'age'
+        dataFilter = userInfo.filter(
+          (item) => String(item[filtering]).indexOf(inputValue) !== -1
+        )
+      } else {
+        dataFilter = userInfo.filter(
+          (item) => item[filtering].indexOf(inputValue) !== -1
+        )
+      }
+      console.log('검색결과', dataFilter)
+      setUserInfo(dataFilter)
+    }
+  }
+
+  return (
+    <div>
+      <Search filterUserInfo={filterUserInfo} searchRef={searchRef} />
+    </div>
+  )
 }
