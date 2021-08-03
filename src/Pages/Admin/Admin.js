7c274457e4e97e74d25fa3026c41312042a95642
psy-g <psykyg@gmail.com>
2021-08-03 18:07:02 +0900

    feat: 새로고침(관리자)

--- src/Pages/Admin/Admin.js
+++ src/Pages/Admin/Admin.js
@@ -2,55 +2,55 @@ import React, { useState, useEffect, useRef } from 'react'
 import styled from 'styled-components/macro'
 import UserTable from './UserTable/UserTable'
 
-import Search from 'Components/Admin/Search'
+import Search from 'Pages/Admin/Search/Search'
 
 export default function Admin() {
   const [userInfo, setUserInfo] = useState([])
+  const [filterInfo, setFilterInfo] = useState([])
   const searchRef = useRef()
 
   useEffect(() => {
-    fetchData()
-  }, [])
-
-  const fetchData = () => {
     fetch('http://localhost:3000/data/users.json')
       .then((res) => res.json())
       .then((res) => {
         setUserInfo(res)
       })
-  }
+  }, [])
 
   const filterUserInfo = (selected) => {
     let inputValue = searchRef.current.value
     let filtering = ''
     let dataFilter = []
-    fetchData()
 
-    // if (e.target.nextElementSibling) {
     if (inputValue.length > 0 && selected !== '선택') {
-      // const inputValue = e.target.nextElementSibling.value
-
-      if (selected === '아이디') filtering = 'email'
+      if (selected === '이메일') filtering = 'email'
       if (selected === '이름') filtering = 'name'
-      if (selected === '나이') {
-        filtering = 'age'
-        dataFilter = userInfo.filter(
-          (item) => String(item[filtering]).indexOf(inputValue) !== -1
-        )
-      } else {
-        dataFilter = userInfo.filter(
-          (item) => item[filtering].indexOf(inputValue) !== -1
-        )
-      }
+      if (selected === '나이') filtering = 'age'
+      dataFilter = userInfo.filter(
+        (item) => item[filtering].indexOf(inputValue) !== -1
+      )
+
       console.log('검색결과', dataFilter)
-      setUserInfo(dataFilter)
+      setFilterInfo(dataFilter)
     }
+  }
+
+  const refreshBtn = () => {
+    setFilterInfo(userInfo)
   }
 
   return (
     <div>
-      <Search filterUserInfo={filterUserInfo} searchRef={searchRef} />
-      <UserTable usersData={userInfo} setUsersData={setUserInfo} />
+      <Search
+        filterUserInfo={filterUserInfo}
+        searchRef={searchRef}
+        refreshBtn={refreshBtn}
+      />
+      <UserTable
+        usersData={userInfo}
+        filterData={filterInfo}
+        setUsersData={setUserInfo}
+      />
       <UserAddButtonWrapper>
         <UserAddButton>사용자 추가</UserAddButton>
       </UserAddButtonWrapper>
--- src/Pages/Admin/UserTable/UserTable.js
+++ src/Pages/Admin/UserTable/UserTable.js
@@ -3,14 +3,18 @@ import styled from 'styled-components'
 import UserCategoryRow from './UserCategoryRow/UserCategoryRow'
 import UserRows from './UserRows/UserRows'
 
-export default function UserTable({ usersData, setUserData }) {
+export default function UserTable({ usersData, filterData, setUserData }) {
   const handleClickTable = () => {
-    console.log(1)
+    console.log('1')
   }
   return (
     <Table onClick={handleClickTable}>
       <UserCategoryRow />
-      <UserRows usersData={usersData} />
+      {filterData.length !== 0 ? (
+        <UserRows usersData={filterData} />
+      ) : (
+        <UserRows usersData={usersData} />
+      )}
     </Table>
   )
 }
