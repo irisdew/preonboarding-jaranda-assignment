96474632152768b2282b59fea903932cd56ff0c9
Cleardew Yoon <ycleardew@gmail.com>
2021-08-04 13:10:14 +0900

    Merge feature/signup to develop (#15)
    * feat : 회원가입 폼 구현, password confirm 일치 확인 기능 추가
    
    * feat : 다음 주소 api 연결, 주소 입력 폼 구현
    
    * refactor :  주소 입력 폼 수정
    
    * refactor : useInput Utils로 분리
    
    * refactor : useInput으로 변경
    
    * fix : 주소 입력 폼 에러 해결
    
    * feat : 카드 입력 모달 창 구현
    
    * feat : 모달에서 입력한 카드 정보 회원가입 form에 전송
    
    * Feature/password validation (#14)
    
    * chore : fontawesomeness 설치
    
    * feat : 비밀번호 유효성 검사 UI
    
    * feat : 비밀번호 유효성 검사 기능 구현
    
    * fix : 오류 수정
    
    * fix : props명 수정
    
    * fix : 주소 관련 상태 Signup 페이지로 이동
    
    Co-authored-by: hami-dev <rinrin.xml@gmail.com>

--- src/Styles/GlobalStyle.js
+++ src/Styles/GlobalStyle.js
@@ -9,6 +9,10 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
     margin: 0;
   }
+
+  html {
+    font-size: 62.5%;
+  }
   
   body {
     font-family: Circular, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, sans-serif;
@@ -21,14 +25,14 @@ const GlobalStyle = createGlobalStyle`
   a, a:hover, a:focus {
     color: inherit;
     text-decoration: none;
-    cursor: pointer;
+    /* cursor: pointer; */
   }
 
   input, button {
     background-color: transparent;
     border: none;
     outline: none;
-    cursor: pointer;
+    /* cursor: pointer; */
   }
 
   ol, ul, li {
