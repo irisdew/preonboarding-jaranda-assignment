73fef94252fd5f1285a236ac2929618d0119167a
irisdew <ycleardew@gmail.com>
2021-08-05 12:33:33 +0900

    feat : 회원가입 페이지 layout 수정

--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -13,6 +13,9 @@ import validation from 'Utils/Validation/Validation'
 import { useInput } from 'Utils/Hooks/useInput'
 import { usePopup } from 'Pages/Signup/usePopup'
 
+import bgImgUrl from 'Assets/Images/bg-sign_up.png'
+import mBgImgUrl from 'Assets/Images/bg-sign-m.png'
+
 export default function Signup() {
   const { isShow, message, toast } = useToast()
 
@@ -108,84 +111,117 @@ export default function Signup() {
   }
 
   return (
-    <Layout>
-      <FormSection>
-        <FormTitle>
-          <div>간편하게 회원가입하고</div>
-          <div>자란다를 이용해보세요</div>
-        </FormTitle>
-        <Input
-          type="text"
-          placeholder="이메일"
-          value={email}
-          onChange={onChangeEmail}
-          onBlur={checkEmail}
-        />
-        <Input
-          type="password"
-          placeholder="비밀번호"
-          value={pass}
-          onChange={onChangePass}
-          onBlur={checkPasswordPolicy}
-        />
-        <PasswordPolicy passPolicy={passPolicy} />
-        <Input
-          type="password"
-          placeholder="비밀번호 확인"
-          onBlur={checkPassWord}
-          value={passConfirm}
-          onChange={onChangePassConfirm}
-        />
-        <Input
-          type="text"
-          name="name"
-          placeholder="이름"
-          value={name}
-          onChange={checkValidation}
-        />
-        <Input
-          type="text"
-          name="age"
-          placeholder="나이"
-          value={age}
-          onChange={checkValidation}
-        />
-        <InputTitle>주소</InputTitle>
-        <Address
-          post={post}
-          setPost={setPost}
-          addr={addr}
-          setAddr={setAddr}
-          extraAddr={extraAddr}
-          setExtraAddr={setExtraAddr}
-          onChangeExtraAddr={onChangeExtraAddr}
-        />
-        <InputTitle>결제 정보</InputTitle>
-        <FlexDiv>
-          <Input type="text" value={cardNum} placeholder="{cardNum}" disabled />
-          <SmallButton clickHandler={openPopup} type="button">
-            카드 입력하기
-          </SmallButton>
-        </FlexDiv>
-        <InputTitle>회원 유형을 선택해주세요</InputTitle>
-        <Radio type="radio" name="role" id="radio_student" />
-        <Label htmlFor="radio_student">학생</Label>
-        <Radio type="radio" name="role" id="radio_parent" />
-        <Label htmlFor="radio_parent">학부모님</Label>
-        <Radio type="radio" name="role" id="radio_teacher" />
-        <Label htmlFor="radio_teacher">선생님</Label>
-        <LongButton clickHandler={onSubmitHandler}>가입하기</LongButton>
-        {showPopup ? (
-          <>
-            <CardPopup onSubmit={onCardSubmit} />
-            <Background onClick={closePopup} />
-          </>
-        ) : null}
-        <Toast message={message} isShow={isShow} />
-      </FormSection>
+    <Layout footerColor="blue">
+      <StyledSection>
+        <h2 className="a11y">회원가입 페이지</h2>
+        <FormSection>
+          <FormTitle>
+            <div>간편하게 회원가입하고</div>
+            <div>자란다를 이용해보세요</div>
+          </FormTitle>
+          <Input
+            type="text"
+            placeholder="이메일"
+            value={email}
+            onChange={onChangeEmail}
+            onBlur={checkEmail}
+          />
+          <Input
+            type="password"
+            placeholder="비밀번호"
+            value={pass}
+            onChange={onChangePass}
+            onBlur={checkPasswordPolicy}
+          />
+          <PasswordPolicy passPolicy={passPolicy} />
+          <Input
+            type="password"
+            placeholder="비밀번호 확인"
+            onBlur={checkPassWord}
+            value={passConfirm}
+            onChange={onChangePassConfirm}
+          />
+          <Input
+            type="text"
+            name="name"
+            placeholder="이름"
+            value={name}
+            onChange={checkValidation}
+          />
+          <Input
+            type="text"
+            name="age"
+            placeholder="나이"
+            value={age}
+            onChange={checkValidation}
+          />
+          <InputTitle>주소</InputTitle>
+          <Address
+            post={post}
+            setPost={setPost}
+            addr={addr}
+            setAddr={setAddr}
+            extraAddr={extraAddr}
+            setExtraAddr={setExtraAddr}
+            onChangeExtraAddr={onChangeExtraAddr}
+          />
+          <InputTitle>결제 정보</InputTitle>
+          <FlexDiv>
+            <Input
+              type="text"
+              value={cardNum}
+              placeholder="{cardNum}"
+              disabled
+            />
+            <SmallButton clickHandler={openPopup} type="button">
+              카드 입력하기
+            </SmallButton>
+          </FlexDiv>
+          <InputTitle>회원 유형을 선택해주세요</InputTitle>
+          <Radio type="radio" name="role" id="radio_student" />
+          <Label htmlFor="radio_student">학생</Label>
+          <Radio type="radio" name="role" id="radio_parent" />
+          <Label htmlFor="radio_parent">학부모님</Label>
+          <Radio type="radio" name="role" id="radio_teacher" />
+          <Label htmlFor="radio_teacher">선생님</Label>
+          <LongButton clickHandler={onSubmitHandler}>가입하기</LongButton>
+          {showPopup ? (
+            <>
+              <CardPopup onSubmit={onCardSubmit} />
+              <Background onClick={closePopup} />
+            </>
+          ) : null}
+          <Toast message={message} isShow={isShow} />
+        </FormSection>
+      </StyledSection>
     </Layout>
   )
 }
+
+const StyledSection = styled.section`
+  position: relative;
+  padding: 19.2rem 0 12.8rem;
+  z-index: 100;
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    display: block;
+    width: 100%;
+    height: 37.7rem;
+    background: url(${bgImgUrl}) no-repeat top right;
+    transform: translateY(-7rem);
+    z-index: -1;
+  }
+  @media screen and ${({ theme }) => theme.device.tablet} {
+    padding: 3.7rem 0 0;
+    &::before {
+      height: 13.7rem;
+      background: url(${mBgImgUrl}) no-repeat top right;
+      transform: translateY(-4rem);
+    }
+  }
+`
 
 const FormSection = styled.form`
   width: 45rem;
@@ -194,7 +230,10 @@ const FormSection = styled.form`
 
 const FormTitle = styled.div`
   font-size: 2.4rem;
-  margin: 10rem 0 3rem;
+  margin-bottom: 3rem;
+  @media screen and ${({ theme }) => theme.device.tablet} {
+    margin-top: 4.8rem;
+  }
 `
 
 export const InputTitle = styled.div`
@@ -213,7 +252,7 @@ export const Input = styled(CustomInput)`
   :focus {
     color: #0085fd;
     border: solid 1px #0085fd;
-    background-color: rgba(0, 133, 253, 0.1);
+    background-color: ${({ theme }) => theme.color.secondaryAlpha};
   }
 `
 
@@ -228,13 +267,13 @@ const Label = styled.label`
 export const LongButton = styled(Button)`
   width: 100%;
   margin-top: 5rem;
-  background-color: #0085fd;
+  background-color: ${({ theme }) => theme.color.secondary};
 `
 
 export const SmallButton = styled(Button)`
   width: 30rem;
   margin-left: 1rem;
-  background-color: #0085fd;
+  background-color: ${({ theme }) => theme.color.secondary};
   border-radius: 0.2rem;
 `
 const Background = styled.div`
